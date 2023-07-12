4d7980169d922c49263b12edb07e8e8acf299a2b
outstandyy <zinchenko.vladislav.1995@gmail.com>
2023-07-12 15:00:14 +0200

    refinements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <p>Search string</p>
+    <input type="text" (input)="setSearchString()">
+    <hr>
+    <ul>
+      <li *ngFor="let user of filteredUsers">{{ user.name }}</li>
+    </ul>
+    <hr>
+    <button (click)="addUser()">Add random user</button>
+  `,
+  standalone: true,
+  imports: [CommonModule],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learn-angular-signals';
+  search = signal('');
 }
